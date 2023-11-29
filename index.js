6f34bc26ea241c4304252440f7c231e94770a5fe
Akem-Ben <akemini.ndaobong@gmail.com>
2023-11-29 23:30:06 +0100

    simple calculator

---
+++ index.js
@@ -0,0 +1,25 @@
+// let input = document.getElementById('input')
+// let button = document.querySelectorAll("button")
+
+// let output = "";
+
+// const calculate = (value) => {
+//     console.log(value)
+//     input.value = output
+// }
+
+// button.forEach(button=>{
+//     button.addEventListener("click", (e) => calculate(e.target.dataset.value))
+// })
+
+
+// button.forEach(button=>{
+//     button.addEventListener("click", (e) => console.log(e.target.dataset.value))
+// })
+// console.log(input, button)
+
+// let arr = [2,4,6,8,10,12]
+// let newArr = []
+// arr.forEach((element)=> newArr.push(element * 2))
+
+// console.log(newArr)
